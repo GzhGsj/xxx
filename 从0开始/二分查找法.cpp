2aab45b8f852527831ac2bb2ad3b04105591dde0
GzhGsj <129638601+GzhGsj@users.noreply.github.com>
2023-04-02 21:33:20 +0800

    Rename tt.cpp to 二分查找法.cpp

---
+++ 从0开始/二分查找法.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<string.h>
+//二分查找法
+/*算法思想与分析：
+	找到数组的最左与最右元素的下标，取平均值，找到中间元素，若中间数比要找的数大，
+	那么就说明要找的数在中间数的左边，那么就再进行取平均找到下一次的中间元素并进行比较，
+	进行该循环 就可以找到要找的值。
+		  实现代码如下
+					 */
+int main() {
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };//要进行查找的数组
+	int find = 7;//要找的数
+	int left = 0;//最左边元素的下标
+	int right = (sizeof(arr) / sizeof(arr[0])) - 1;//最右边元素的下标
+	while (1)   
+	{
+		int mid = (right + left) / 2;//取平均找到中间元素的下标
+		if (find > arr[mid]) {
+			left = mid + 1;
+		}
+		else if (find < arr[mid]) {
+			right = mid - 1;
+		}
+		else {
+			printf("要找的元素下标为：%d", mid);
+			break;
+		}
+	}
+	return 0;
+}
